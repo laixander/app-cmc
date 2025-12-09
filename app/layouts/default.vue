6d4bb6d188677d4f978c82ddc7c3738482767d80
laixander <laixander@gmail.com>
2025-12-09 23:39:58 +0800

    updates

---
+++ app/layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="min-h-screen grid">
+        <slot></slot>
+    </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="p-6 flex justify-center items-center">
+        <UCard class="max-w-sm" :ui="{header:'flex justify-between items-center'}">
+            <template #header>
+                <div class="font-semibold">
+                    Simple Application
+                </div>
+                <UColorModeButton />
+            </template>
+            <div class="text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aspernatur consequuntur iste dolor repudiandae accusantium ut molestias impedit provident maxime modi quaerat eum, sapiente qui, omnis vero dignissimos optio voluptas.
+            </div>
+            <template #footer>
+                <UButton label="Continue" size="lg" block to="/dashboard" />
+            </template>
+        </UCard>
+    </div>
+</template>
